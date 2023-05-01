86534327d86e901520775cdf410836e5de778257
Kni9t <clone111@bk.ru>
2023-05-02 00:02:37 +0300

    Restructuring
    Allocation mail logic to a new class

--- PatternGenerator/Form1.cs
+++ PatternGenerator/Form1.cs
@@ -19,123 +19,34 @@ namespace PatternGenerator
 
         const int SIZE = 5;
         int MapHeight, MapWidth;
-        bool Mode = true, Time = true;
-
-        bool[,] Map; // Height на Width
+        SquareGrid SG;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             MapHeight = pictureBox1.Height / SIZE;
             MapWidth = pictureBox1.Width / SIZE;
 
-            Map = new bool[MapHeight, MapWidth];
-
-            for (int i = 0; i < Map.GetLength(0); i++)
-                for (int j = 0; j < Map.GetLength(1); j++) Map[i, j] = false;
-
-            timer1.Enabled = Time;
-        }
-        int StringCheck(int id)
-        {
-            if (id >= MapHeight) return 0;
-            if (id < 0) return (MapHeight - 1);
-            return id;
-        }
-        int ColumCheck(int id)
-        {
-            if (id >= MapWidth) return 0;
-            if (id < 0) return (MapWidth - 1);
-            return id;
-        }
-        void UpdatePrint()
-        {
-            Bitmap BitMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            Graphics G = Graphics.FromImage(BitMap);
-
-            for (int i = 0; i < MapHeight; i++)
-                for (int j = 0; j < MapWidth; j++)
-                    if (Map[i, j]) G.FillRectangle(Brushes.White, 0 + SIZE * j, 0 + SIZE * i, SIZE, SIZE);
-                    else G.FillRectangle(Brushes.Black, 0 + SIZE * j, 0 + SIZE * i, SIZE, SIZE);
-
-            pictureBox1.Image = BitMap;
-            GC.Collect();
+            SG = new SquareGrid(MapHeight, MapWidth, SIZE);
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            Random R = new Random();
-            if (e.Button == MouseButtons.Left)
-            {
-                int PositionX = e.X / SIZE, PositionY = e.Y / SIZE;
-
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX - 1)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX + 1)] = true;
-
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX - 1)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX + 1)] = true;
-
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX - 1)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX)] = true;
-                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX + 1)] = true;
-            }
-
-            if (e.Button == MouseButtons.Right) Map[StringCheck(e.Y / SIZE), ColumCheck(e.X / SIZE)] = true;
+            SG.MouseActive(e);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateLogic();
+            SG.UpdateLogic();
         }
-
+         
         private void timer2_Tick(object sender, EventArgs e)
         {
-            UpdatePrint();
-        }
+            Bitmap BitMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics G = Graphics.FromImage(BitMap);
 
-        void UpdateLogic()
-        {
-            bool[,] Buf = new bool[MapHeight, MapWidth];
+            SG.UpdatePrint(G);
 
-            for (int i = 0; i < MapHeight; i++)
-            {
-                for (int j = 0; j < MapWidth; j++)
-                {
-                    int bufcount = 0;
-
-                    if (Map[StringCheck(i - 1), ColumCheck(j - 1)]) bufcount++;
-                    if (Map[StringCheck(i - 1), ColumCheck(j)]) bufcount++;
-                    if (Map[StringCheck(i - 1), ColumCheck(j + 1)]) bufcount++;
-
-                    if (Map[StringCheck(i), ColumCheck(j - 1)]) bufcount++;
-                    if (Map[StringCheck(i), ColumCheck(j + 1)]) bufcount++;
-
-                    if (Map[StringCheck(i + 1), ColumCheck(j - 1)]) bufcount++;
-                    if (Map[StringCheck(i + 1), ColumCheck(j)]) bufcount++;
-                    if (Map[StringCheck(i + 1), ColumCheck(j + 1)]) bufcount++;
-
-                    if (Map[i, j] == false) // Стандарт
-                    {
-                        if (bufcount == 3) Buf[i, j] = true;
-                        else Buf[i, j] = false;
-                    }
-                    else
-                    {
-                        if ((bufcount == 2) || (bufcount == 3)) Buf[i, j] = true;
-                        else Buf[i, j] = false;
-                    }
-                }
-            }
-
-            Map = Buf;
-        }
-        void ClearMap()
-        {
-            Map = new bool[MapHeight, MapWidth];
-
-            for (int i = 0; i < MapHeight; i++)
-                for (int j = 0; j < MapWidth; j++) Map[i, j] = false;
+            pictureBox1.Image = BitMap;
         }
     }
 }
---
+++ PatternGenerator/SquareGrid.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatternGenerator
+{
+    public class SquareGrid
+    {
+        bool[,] Map; // Height на Width
+        int SquareSize;
+
+        public SquareGrid(int MapHeight, int MapWidth, int SquareSize = 5)
+        {
+            Map = new bool[MapHeight, MapWidth];
+            this.SquareSize = SquareSize;
+
+            for (int i = 0; i < Map.GetLength(0); i++)
+                for (int j = 0; j < Map.GetLength(1); j++) Map[i, j] = false;
+        }
+        int StringCheck(int id)
+        {
+            if (id >= Map.GetLength(0)) return 0;
+            if (id < 0) return (Map.GetLength(0) - 1);
+            return id;
+        }
+        int ColumCheck(int id)
+        {
+            if (id >= Map.GetLength(1)) return 0;
+            if (id < 0) return (Map.GetLength(1) - 1);
+            return id;
+        }
+        public void UpdatePrint(Graphics G)
+        {
+            for (int i = 0; i < Map.GetLength(0); i++)
+                for (int j = 0; j < Map.GetLength(1); j++)
+                    if (Map[i, j]) G.FillRectangle(Brushes.White, 0 + SquareSize * j, 0 + SquareSize * i, SquareSize, SquareSize);
+                    else G.FillRectangle(Brushes.Black, 0 + SquareSize * j, 0 + SquareSize * i, SquareSize, SquareSize);
+        }
+        public void UpdateLogic()
+        {
+            bool[,] Buf = new bool[Map.GetLength(0), Map.GetLength(1)];
+
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    int bufcount = 0;
+
+                    if (Map[StringCheck(i - 1), ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i - 1), ColumCheck(j)]) bufcount++;
+                    if (Map[StringCheck(i - 1), ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[StringCheck(i), ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i), ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[StringCheck(i + 1), ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i + 1), ColumCheck(j)]) bufcount++;
+                    if (Map[StringCheck(i + 1), ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[i, j] == false)
+                    {
+                        if (bufcount == 3) Buf[i, j] = true;
+                        else Buf[i, j] = false;
+                    }
+                    else
+                    {
+                        if ((bufcount == 2) || (bufcount == 3)) Buf[i, j] = true;
+                        else Buf[i, j] = false;
+                    }
+                }
+            }
+
+            Map = Buf;
+        }
+        public void MouseActive(MouseEventArgs e)
+        {
+            Random R = new Random();
+            if (e.Button == MouseButtons.Left)
+            {
+                int PositionX = e.X / SquareSize, PositionY = e.Y / SquareSize;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1), ColumCheck(PositionX + 1)] = true;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY), ColumCheck(PositionX + 1)] = true;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1), ColumCheck(PositionX + 1)] = true;
+            }
+
+            if (e.Button == MouseButtons.Right) Map[StringCheck(e.Y / SquareSize), ColumCheck(e.X / SquareSize)] = true;
+        }
+        public void ClearMap()
+        {
+            Map = new bool[Map.GetLength(0), Map.GetLength(1)];
+
+            for (int i = 0; i < Map.GetLength(0); i++)
+                for (int j = 0; j < Map.GetLength(1); j++) Map[i, j] = false;
+        }
+    }
+}
